2331020a78069cc3f077e502cea6d2cf2c8380f9
shciklgruber <shcikshcik@vk.com>
2025-12-15 17:14:02 +0300

    Доработка task, создание Variant.cs

---
+++ EGE_Generator_WPF/Variant.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace EgeGenerator.Models
+{
+    public class Variant
+    {
+        public string Name { get; set; } = string.Empty;
+        public List<EgeTask> Tasks { get; set; } = new List<EgeTask>();
+        public string Path { get; set; } = string.Empty;
+        public bool IsComplete => Tasks.Count == 27;
+    }
+}
