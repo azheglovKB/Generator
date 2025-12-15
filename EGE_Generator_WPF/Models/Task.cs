3f951544d2627389db1584a4377cd5806de4bee3
shciklgruber <shcikshcik@vk.com>
2025-12-15 17:08:10 +0300

    Создание модели с типами заданий task

---
+++ EGE_Generator_WPF/Models/Task.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGE_Generator_WPF.Models
+{
+    internal class Task
+    {
+    }
+}
